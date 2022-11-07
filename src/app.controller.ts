e6f884d418c056c8a55be697de64e16499e790a5
LJS243 <lohanjs@outlook.com>
2022-11-07 16:18:46 +0200

    feat: get current price and 24h percentage change

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@ import {
   CacheInterceptor,
   Controller,
   Get,
+  Param,
   UseInterceptors,
 } from '@nestjs/common';
 import { AppService } from './app.service';
@@ -16,48 +17,13 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('bitcoin')
-  getBitcoin() {
-    return this.appService.getBitcoin();
+  @Get('/:denom')
+  getChartData(@Param('denom') denom: string) {
+    return this.appService.getChartData(denom);
   }
 
-  @Get('ethereum')
-  getEthereum() {
-    return this.appService.getEthereum();
-  }
-
-  @Get('matic-wormhole')
-  getMaticWormhole() {
-    return this.appService.getMaticWormhole();
-  }
-
-  @Get('ixo')
-  getIxo() {
-    return this.appService.getIxo();
-  }
-
-  @Get('toucan-protocol-nature-carbon-tonne')
-  getToucanProtocolNatureCarbonTonne() {
-    return this.appService.getToucanProtocolNatureCarbonTonne();
-  }
-
-  @Get('toucan-protocol-base-carbon-tonne')
-  getToucanProtocolBaseCarbonTonne() {
-    return this.appService.getToucanProtocolBaseCarbonTonne();
-  }
-
-  @Get('cosmos')
-  getCosmos() {
-    return this.appService.getCosmos();
-  }
-
-  @Get('regen')
-  getRegen() {
-    return this.appService.getRegen();
-  }
-
-  @Get('osmosis')
-  getOsmosis() {
-    return this.appService.getOsmosis();
+  @Get('/:denom/price')
+  getPrice(@Param('denom') denom: string) {
+    return this.appService.getPrice(denom);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -13,75 +13,20 @@ export class AppService {
     return 'API Running';
   }
 
-  async getBitcoin() {
-    const denom = 'bitcoin';
+  async getChartData(denom: string) {
     const res = await axios.get(
       `https://api.coingecko.com/api/v3/coins/${denom}/market_chart?vs_currency=${currency}&days=${days}`,
     );
     return res.data;
   }
 
-  async getEthereum() {
-    const denom = 'ethereum';
+  async getPrice(denom: string) {
     const res = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${denom}/market_chart?vs_currency=${currency}&days=${days}`,
+      `https://api.coingecko.com/api/v3/coins/${denom}`,
     );
-    return res.data;
-  }
-
-  async getMaticWormhole() {
-    const denom = 'matic-wormhole';
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${denom}/market_chart?vs_currency=${currency}&days=${days}`,
-    );
-    return res.data;
-  }
-
-  async getIxo() {
-    const denom = 'ixo';
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${denom}/market_chart?vs_currency=${currency}&days=${days}`,
-    );
-    return res.data;
-  }
-
-  async getToucanProtocolNatureCarbonTonne() {
-    const denom = 'toucan-protocol-nature-carbon-tonne';
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${denom}/market_chart?vs_currency=${currency}&days=${days}`,
-    );
-    return res.data;
-  }
-
-  async getToucanProtocolBaseCarbonTonne() {
-    const denom = 'toucan-protocol-base-carbon-tonne';
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${denom}/market_chart?vs_currency=${currency}&days=${days}`,
-    );
-    return res.data;
-  }
-
-  async getCosmos() {
-    const denom = 'cosmos';
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${denom}/market_chart?vs_currency=${currency}&days=${days}`,
-    );
-    return res.data;
-  }
-
-  async getRegen() {
-    const denom = 'regen';
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${denom}/market_chart?vs_currency=${currency}&days=${days}`,
-    );
-    return res.data;
-  }
-
-  async getOsmosis() {
-    const denom = 'osmosis';
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${denom}/market_chart?vs_currency=${currency}&days=${days}`,
-    );
-    return res.data;
+    return {
+      price: res.data.market_data.current_price.usd,
+      change: res.data.market_data.price_change_percentage_24h,
+    };
   }
 }
